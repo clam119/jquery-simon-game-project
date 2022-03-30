d44c0503852cbdfe8f9962cff17d354b6949215d
Clam119 <Christopher.lam119@gmail.com>
2022-03-30 21:22:41 +0100

    Trying to implement fade in/out animations.

--- game.js
+++ game.js
@@ -1,14 +1,35 @@
+var userClickedPattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio = new Audio("/sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+    playSound(randomChosenColour);
 }
 
-$(".btn").on("click", nextSequence);
-$("button").click(nextSequence);
+
+
+
+
+$(".btn").click(function () {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    $("#" + userChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
+
+function animatePress(currentColour) {
+    $(this).addClass(pressed);
+    
+}
+
+// $(".btn").on("click", nextSequence);
